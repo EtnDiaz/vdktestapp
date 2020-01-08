a645a69b3815cfeb6d0a247bb310f22509d76667
EtnDiaz <nekt7777@gmail.com>
2020-01-08 21:43:53 +1000

    Updates

---
+++ precache-manifest.deca67ffcb40f5403f36f0356f9f267c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d523c40946e9a4fdcc4f083f66e0ba62",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6a2fff9d657b0de5287",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "da5b90be78b95738b0bb",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b6a2fff9d657b0de5287",
+    "url": "./static/js/2.91002dcf.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "./static/js/2.91002dcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da5b90be78b95738b0bb",
+    "url": "./static/js/main.fc7d1ffc.chunk.js"
+  },
+  {
+    "revision": "be6706b8ce731ce2deb9",
+    "url": "./static/js/runtime-main.4b0d7a08.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
